9db516b40b9ace25768f5f295507f4647f61fcb6
Pawick69 <Patrick2730@gmail.com>
2023-10-13 15:13:56 +0200

    Første move program test

---
+++ move.js
@@ -0,0 +1,51 @@
+let y = 10; let r = 200; g = 180; b = 0;
+let ned = true;
+let rystet = 0;
+let flyttet = 0;
+let tal = 5;
+
+function setup() {
+    canvas = createCanvas(300, 550);
+    textSize(24);
+    // giver canvas border på 2 pixel, 
+    // og sørger derefter for at kanten tælles med i width
+    canvas.elt.style.border = '5px solid black';
+    canvas.elt.style.boxSizing = 'border-box';
+    canvas.elt.style.borderRadius = '20px';
+
+    //canvas.parent('#beholder');
+    // gør canvas-elementet responsivt til skærmbredden
+    canvas.elt.style.width = '100%';
+    canvas.elt.style.height = '100%';
+
+    //bemærk at noden skal pakkes ud via .elt
+    //const parentDiv = select('#beholder').elt;
+    //const p = select('#test1').elt;
+    // indsæt canvas i ny position i rækkefølgen af elementer i div'en beholder
+    //parentDiv.insertBefore(canvas.elt, p);
+    //let overskrift = document.querySelector('#tal');
+    //console.log(overskrift);
+}
+
+function draw() {
+    if (ned)
+        y++;
+    else
+        y--;
+    if (y >= height || y <= 0)
+        ned = !ned;
+    if (accelerationX > 70) {
+        
+        if(rystet%2 == 0)
+        ned = !ned;
+        rystet++;
+    }
+    
+text('tæl op: ' + str(rystet*5), 50, height-100);
+text('tæl op: ' + str(flyttet*5),50, height-50);
+}
+
+function deviceMoved(){
+    flyttet++;
+
+}
