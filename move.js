5334794a48f8224c8ee9acd312125426e130b762
Pawick69 <Patrick2730@gmail.com>
2023-11-03 10:03:17 +0100

    Update move.js

--- move.js
+++ move.js
@@ -1,12 +1,11 @@
 let rystet = 0;
-let tal = 5;
 let sidsteKlik = 0;
 
 const mq = window.matchMedia("(max-width: 480px)");
 
 function setup() {
     pixelDensity(1);
-    canvas = createCanvas((mq.matches) ? windowWidth : (windowHeight - 100) / 20 * 10.5, (windowHeight - 100));
+    canvas = createCanvas((mq.matches) ? windowWidth : (windowHeight) / 20 * 10.5, (windowHeight));
     canvas.elt.style.border = '5px solid black';
     canvas.elt.style.boxSizing = 'border-box';
     canvas.elt.style.borderRadius = '20px';
@@ -23,15 +22,17 @@ function setup() {
 
 function draw() {
     console.log(windowWidth, windowHeight);
-    resizeCanvas((mq.matches) ? windowWidth : (windowHeight - 100) / 20 * 10.5, (windowHeight - 100));
+    resizeCanvas((mq.matches) ? windowWidth : (windowHeight) / 20 * 10.5, (windowHeight));
     background(173, 216, 230);
-    if (accelerationX > 70 && millis() - sidsteKlik > 500) {
+    if (accelerationX > 70 && millis() - sidsteKlik > 200) {
         rystet += 5;
         sidsteKlik = millis();
     }
 
-    textSize(50)
-    text("5 TABELLEN", width/2, 100);
+    textSize(47)
+    text("5 TABELLEN", width/2, 300);
+    text("VÆLG EN TABEL", width/2, 120);
+    text("GÅ UDENFOR!", width/2, 50);
 
     textSize(100)
     textAlign(CENTER, CENTER);  
@@ -41,5 +42,5 @@ function draw() {
 }
 
 function windowResized() {
-    resizeCanvas(mq.matches) ? windowWidth : (windowHeight - 100) / 20 * 10.5, (windowHeight - 100);
+    resizeCanvas(mq.matches) ? windowWidth : (windowHeight) / 20 * 10.5, (windowHeight);
 }
